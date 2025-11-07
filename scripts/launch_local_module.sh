e8a49f3befa78673e5bea78ee63219d068f0ee7a
Marc Duran Olivé <marc.duran@earthrover.farm>
2025-11-07 16:20:03 +0100

    Fixed ROSCONSOLE_FORMAT env var on the services

--- scripts/launch_local_module.sh
+++ scripts/launch_local_module.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export ROS_LOG_DIR="/home/earth/data/log"
-export ROSCONSOLE_FORMAT="[${severity}] [${time}] [${node}]: ${message}"
+export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
 export DARKNET_PATH="/home/earth/libs/earth_rover_darknet"
 export PATH="/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 export LD_LIBRARY_PATH="/usr/local/cuda/targets/aarch64-linux/lib:/usr/local/lib"
